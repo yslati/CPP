df58141e5f44c053269af649c37a7c5c1dbd8d95
Yassin Slati <yslati@e2r6p6.1337.ma>
2021-04-09 10:45:07 +0100

    save

--- D08/ex00/easyfind.hpp
+++ D08/ex00/easyfind.hpp
@@ -1,9 +1,25 @@
 #ifndef EASYFIND_H
 # define EASYFIND_H
 
+#include <exception>
+#include <list>
+#include <vector>
+#include <iostream>
+
+class NotFound: public std::exception {
+	public:
+		virtual const char* what() const throw() {
+			return ("Not Found");
+		}
+}
+
 template<typename T>
-void	eeasyfind(T *arr, int i) {
-	
+int		easyfind(T & arr, int find) {
+	typename T::iterator it;
+	it = std::find(T.begin(), T.end(), find);
+	if (it != T.end())
+		return 1;
+	throw
 }
 
 #endif
--- D08/ex00/main.cpp
+++ D08/ex00/main.cpp
@@ -2,8 +2,15 @@
 
 
 int main() {
-
+	std::list<int> l;
+	for (int i = 0; i < 6; i++)
+		l.push_back(i);
+	std::list<int>::iterator it;
+	for (it = l.begin(); it != l.end(); it++)
+		std::cout << *it << std::endl;
+	std::list<int>::iterator a = std::find(l.begin(), l.end(), 10);
+	std::cout << "hel : " << *a << std::endl;
 	
-
+	std::cout << easyfind(l, 5) << std::endl;
 	return 0;
 }
